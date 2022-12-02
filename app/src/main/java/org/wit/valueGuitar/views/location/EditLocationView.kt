a2abdc934cc049ed36f60eee04975ff5fcc5375b
owencorrigan <owencorrigan76@gmail.com>
2022-12-02 17:12:25 +0000

    EditLocation fixed, return to GuitarView

--- app/src/main/java/org/wit/valueGuitar/views/location/EditLocationView.kt
+++ app/src/main/java/org/wit/valueGuitar/views/location/EditLocationView.kt
@@ -7,40 +7,44 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.Marker
 import org.wit.valueGuitar.R
+import org.wit.valueGuitar.databinding.ActivityMapBinding
 import org.wit.valueGuitar.models.Location
 
 
-class EditLocationView : AppCompatActivity(), OnMapReadyCallback,
+class EditLocationView : AppCompatActivity(),
     GoogleMap.OnMarkerDragListener,
     GoogleMap.OnMarkerClickListener {
-
+    private lateinit var binding: ActivityMapBinding
     private lateinit var map: GoogleMap
     lateinit var presenter: EditLocationPresenter
     var location = Location()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
-        presenter = EditLocationPresenter(this)
-        location = intent.extras?.getParcelable<Location>("location")!!
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-        presenter.initMap(map)
+        presenter = EditLocationPresenter(this)
+
+        location = intent.extras?.getParcelable<Location>("location")!!
+        binding.forGoogleMap.onCreate(savedInstanceState)
+        binding.forGoogleMap.getMapAsync{
+            it.setOnMarkerDragListener(this)
+            it.setOnMarkerClickListener(this)
+            presenter.initMap(it)
+        }
     }
 
     override fun onMarkerDragStart(marker: Marker) {
     }
 
     override fun onMarkerDrag(marker: Marker) {
+        binding.lat.setText("%.6f".format(marker.position.latitude))
+        binding.lng.setText("%.6f".format(marker.position.longitude))
     }
 
     override fun onMarkerDragEnd(marker: Marker) {
-        presenter.doUpdateLocation(marker.position.latitude,marker.position.longitude, map.cameraPosition.zoom)
+        presenter.doUpdateLocation(marker.position.latitude,marker.position.longitude)
     }
 
     override fun onMarkerClick(marker: Marker): Boolean {
@@ -51,5 +55,29 @@ class EditLocationView : AppCompatActivity(), OnMapReadyCallback,
     override fun onBackPressed() {
         presenter.doOnBackPressed()
 
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.forGoogleMap.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.forGoogleMap.onLowMemory()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.forGoogleMap.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.forGoogleMap.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.forGoogleMap.onSaveInstanceState(outState)
     }
 }
